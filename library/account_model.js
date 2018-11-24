163f3a38fba151cd4ede6183d811ce092d9d1120
Matthew LeFevre <matthewlefevre95@gmail.com>
2018-11-24 11:54:08 -0700

    login account service

--- library/account_model.js
+++ library/account_model.js
@@ -28,7 +28,7 @@ exports.getAccountInfo = async function (accountName) {
     const response = await pool.query(`SELECT * FROM account WHERE accountName = '${accountName}'`);
 
     if (response.rowCount === 1) {
-      return response.fields[field];
+      return response.fields;
     } else {
       return false;
     }
